9271e38fe9bc666a9131d35ec84f408eb8d676ea
santiago <santiagovillacano48@gmail.com>
2025-03-02 22:02:10 -0500

    V.1.1

---
+++ Ultra/ultra.js
@@ -0,0 +1,6 @@
+function i1(){
+    let estatura=prompt("digite su altura (cm):")
+
+if (estatura >=150) {alert("Puede ingresar a la atraccion")} 
+    else{alert("No puede ingresar a la atraccion")}
+}
